50f42ff111717af84983472a4ece3062f745a675
WahomeJeremy <wahome@nisria.co>
2025-04-09 09:00:20 +0300

    Exercises

---
+++ app.py
@@ -0,0 +1,39 @@
+print('Hello World!')
+
+x = 5.35 
+print(type(x))
+
+y = 1
+print(type(y))
+
+z = 2 + 3
+print(type(z))
+
+
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name 
+full_name2 = f"{first_name} {last_name}"
+
+print(first_name)
+print(last_name)
+print(full_name)
+
+my_range = range(50)
+print(list(my_range))
+
+my_dict = {
+    "name": "John Doe"
+}
+
+print(my_dict)
+print(type(my_dict))
+
+my_set = {'sugar', 'salt'}
+print(my_set)
+print(type(my_set))
+
+
+
+
+
